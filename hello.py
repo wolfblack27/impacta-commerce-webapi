c54eafd8741056041aacf13571059b905ef73d22
wolfblack27 <tigowolfblack27@gmail.com>
2023-10-27 20:53:37 -0300

    criacao da rota products

--- hello.py
+++ hello.py
@@ -1,7 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World! OI mundão</p>"
+
+@app.route("/products")
+def products():
+    response = jsonify([
+
+        {"title":" Caneca Personalizada de Porcelanato - Backend",
+         "amount":123.45,
+         "installments":{"number":3,"total": 41.15, "hasFee": True},
+         },
+
+        {"title":" Caneca de Tulipa - Backend",
+         "amount":123.45,
+         "installments":{"number":3,"total": 41.15},
+         },
+       
+       {"title":" Caneca de Barro - Backend",
+         "amount":123.45,
+         "installments":{"number":3,"total": 41.15},
+         },
+
+         {"title":" Caneca de Ouro - Backend",
+         "amount":123.45,
+         "installments":{"number":3,"total": 41.15},
+         },
+
+
+    ])
+
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000)
